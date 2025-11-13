b66754c18aa9b8d094eaf555e755a2ba3d5cff16
ProSamhacker <you@example.com>
2025-11-13 21:05:59 +0530

    format

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,16 @@
+import tailwindcss from "@tailwindcss/postcss";
+import typography from "@tailwindcss/typography";
+
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: [
+    tailwindcss({
+      // This is where you can add your tailwind.config.ts path if needed
+      // config: './tailwind.config.ts', 
+      
+      // Add the typography plugin here
+      plugins: [typography()],
+    }),
+  ],
 };
 
 export default config;
